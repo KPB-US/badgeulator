6a8d704d69e7a2e5f274f9c7aa3c129fb899c246
Stryker McLane <strykermclane@gmail.com>
2024-06-06 12:08:33 -0800

    added error handling to artifact props

--- app/models/prawn_artifact_fill_gradient.rb
+++ app/models/prawn_artifact_fill_gradient.rb
@@ -1,5 +1,9 @@
 class PrawnArtifactFillGradient < PrawnArtifact
   def render(layout_guides)
-    prawn.fill_gradient props[:from], props[:to], props[:color1], props[:color2]
+    if props.key?(:from) && props.key?(:to) && props.key?(:color1) && props.key?(:color2) 
+      prawn.fill_gradient props[:from], props[:to], props[:color1], props[:color2]
+    else
+      prawn.text artifact.name + " id " + artifact.id.to_s + ' needs from, to, color1 and color2.', color: 'FF0000', size: 10
+    end
   end
 end
--- app/models/prawn_artifact_stroke_rectangle.rb
+++ app/models/prawn_artifact_stroke_rectangle.rb
@@ -1,9 +1,12 @@
 class PrawnArtifactStrokeRectangle < PrawnArtifact
   def render(layout_guides)
-    prawn.line_width(props[:line_width])
-    color = prawn.stroke_color
-    prawn.stroke_color = props[:color] if props.key?(:color)
-    prawn.stroke_rounded_rectangle props[:at], props[:width], props[:height], props[:radius].to_i
-    prawn.stroke_color = color
+    if props.key?(:at) && props.key?(:height) && props.key?(:width) && props.key?(:line_width) && props.key?(:radius) && props.key?(:color)
+      prawn.line_width(props[:line_width])
+      prawn.stroke_color = props[:color] 
+      prawn.stroke_rounded_rectangle props[:at], props[:width], props[:height], props[:radius].to_i
+    else
+      prawn.text artifact.name + " id " + artifact.id.to_s + ' needs at, width, height, radius, line_width, color.', color: 'FF0000', size: 10
+    end
+
   end
 end
