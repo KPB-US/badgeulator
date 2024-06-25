0550c6c278ceb907901d707e9754a2fa60ae41cb
Stryker McLane <strykermclane@gmail.com>
2024-06-25 15:55:02 -0800

    added print queue partial

---
+++ app/controllers/printqueue_controller.rb
@@ -0,0 +1,15 @@
+class PrintqueueController < ApplicationController
+  # def queue
+  #   cmd = "lpq"
+  #   output = `#{cmd}` 
+  #   output.to_s
+  #   # render html: output.to_s.html_safe 
+  #   cmd = "lpstat -l"
+  #   output = `#{cmd}` 
+  #   a = output.split("\n") 
+  #   a.each do |n| 
+  #     n.to_s 
+  #     # render html: n.to_s.html_safe 
+  #   end 
+  # end
+end
