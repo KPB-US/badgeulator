d0483518d35848d833e27369c750f6cff5492c5d
Stryker McLane <strykermclane@gmail.com>
2024-06-26 15:27:10 -0800

    added job count partial

--- app/controllers/printqueue_controller.rb
+++ app/controllers/printqueue_controller.rb
@@ -1,15 +1,9 @@
 class PrintqueueController < ApplicationController
-  # def queue
-  #   cmd = "lpq"
-  #   output = `#{cmd}` 
-  #   output.to_s
-  #   # render html: output.to_s.html_safe 
-  #   cmd = "lpstat -l"
-  #   output = `#{cmd}` 
-  #   a = output.split("\n") 
-  #   a.each do |n| 
-  #     n.to_s 
-  #     # render html: n.to_s.html_safe 
-  #   end 
-  # end
+  def jobcount
+    cmd = "lpstat"
+    output = `#{cmd}` 
+    a = output.split("\n") 
+    count = a.count
+    return count
+  end
 end
